417b28efef3c5bdaa2316493befe75d16d151f4b
jaykrishna12345 <108941785+jaykrishna12345@users.noreply.github.com>
2022-07-17 23:26:14 +0530

    even and odd numbers

---
+++ even_and_odd_numers.py
@@ -0,0 +1,14 @@
+list1 = [1,2,3,4,5,6,7,8,9,]
+ 
+even_count= 0
+odd_count = 0
+ 
+for num in list1:
+    if num % 2 == 0:
+        even_count += 1
+ 
+    else:
+        odd_count += 1
+         
+print("number of even numbers: ", even_count)
+print("number of odd numbers: ", odd_count)
